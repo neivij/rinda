0bf0dc82245eeb070ba407ca60a70e8efbaaeafc
neivij <86239338+neivij@users.noreply.github.com>
2022-09-12 10:17:12 +0300

    fix bug + edit comment

--- rinda/Src/main.c
+++ rinda/Src/main.c
@@ -3,10 +3,10 @@
 #include "main.h"
 #include <stm32f4xx.h>
 
-uint8_t pressed = 0;
+volatile uint8_t pressed = 0;
 
-/*estimated delay*/
-void Delay(uint32_t time)
+/*estimated delay (time shouldn' t exceed 32bit max value)*/
+void Delay(volatile uint32_t time)
 {
     for (; time > 0; time--);
 }
@@ -19,8 +19,8 @@ int main()
 {
 
     /*enable RCC for GPIO A and B*/
-    RCC->AHB1ENR = GPIOAEN;
-    RCC->AHB1ENR = GPIOBEN;
+    RCC->AHB1ENR |= GPIOAEN;
+    RCC->AHB1ENR |= GPIOBEN;
 
     /*clear GPIO A and B mode registers*/
     uint32_t *reg = (uint32_t *)0x40020400;
@@ -31,29 +31,29 @@ int main()
     *reg ^= 0xffffffff;
 
     /*set GPIO A1-7 and B0-1 mode as output*/
-    GPIOA->MODER = GPIO_PIN_1_OUT;
-    GPIOA->MODER = GPIO_PIN_2_OUT;
-    GPIOA->MODER = GPIO_PIN_3_OUT;
-    GPIOA->MODER = GPIO_PIN_4_OUT;
-    GPIOA->MODER = GPIO_PIN_5_OUT;
-    GPIOA->MODER = GPIO_PIN_6_OUT;
-    GPIOA->MODER = GPIO_PIN_7_OUT;
-    GPIOB->MODER = GPIO_PIN_0_OUT;
-    GPIOB->MODER = GPIO_PIN_1_OUT;
+    GPIOA->MODER |= GPIO_PIN_1_OUT;
+    GPIOA->MODER |= GPIO_PIN_2_OUT;
+    GPIOA->MODER |= GPIO_PIN_3_OUT;
+    GPIOA->MODER |= GPIO_PIN_4_OUT;
+    GPIOA->MODER |= GPIO_PIN_5_OUT;
+    GPIOA->MODER |= GPIO_PIN_6_OUT;
+    GPIOA->MODER |= GPIO_PIN_7_OUT;
+    GPIOB->MODER |= GPIO_PIN_0_OUT;
+    GPIOB->MODER |= GPIO_PIN_1_OUT;
 
     /*set user button(PA 0) as input*/
-    GPIOB->MODER = GPIO_PIN_0_IN;
+    GPIOB->MODER |= GPIO_PIN_0_IN;
     /*enable SYSCFG clock*/
-    RCC->APB2ENR = SYSCFG_EN;
+    RCC->APB2ENR |= SYSCFG_EN;
     /*set PA0 as source input for EXTI0) */
     reg = (uint32_t *)((uint32_t)0x40013800 + SYSCFG_EXTICR1);
     *reg |= 0xf;
     *reg ^= 0xf; // first 4 LSB set to 0000
 
     /*enable rising edge trigger for exti0*/
-    EXTI->RTSR = RT0;
+    EXTI->RTSR |= RT0;
     /*set exti0 as not masked*/
-    EXTI->IMR = EXTI0_NOTMASKED;
+    EXTI->IMR |= EXTI0_NOTMASKED;
 
     /*set interrupt priority to highest and enable the Interrupt */
     NVIC_SetPriority(EXTI0_IRQn, 0);
